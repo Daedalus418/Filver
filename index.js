910b9a9e61e312e2142efb2a77275de73122a201
daedalus418 <morin.gaelle@gmail.com>
2017-01-09 19:31:51 +0100

    index.js created

---
+++ index.js
@@ -0,0 +1,42 @@
+let ticket = [
+	{ "nom": "Fromage Président Camembert",
+		"prix": 2.58,
+		"tva": 1,
+		"quantité": 2
+	},
+	{ "nom": "Beurre La Laitière",
+		"prix": 1.70,
+		"tva": 1,
+		"quantité": 1
+	},
+	{ "nom": "Whiskey J&B",
+		"prix": 9.42,
+		"tva": 2,
+		"quantité": 2
+	},
+	{ "nom": "Croquette Animalières Whiskas",
+		"prix": 3.73,
+		"tva": 2,
+		"quantité": 1
+	},
+	{ "nom": "Gel Douche Dove for Men 0%",
+		"prix": 2.31,
+		"tva": 2,
+		"quantité": 3
+	},
+	{ "nom": "Chocolat Allegé Milka",
+		"prix": 1.28,
+		"tva": 2,
+		"quantité": 5
+	},
+	{ "nom": "Vin Chardonnay Suisse",
+		"prix": 7.99,
+		"tva": 2,
+		"quantité": 1
+	},
+	{ "nom": "Mort SUB Belg",
+		"prix": 12.99,
+		"tva": 2,
+		"quantité": 5
+	}
+];
